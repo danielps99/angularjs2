758c6d61e47d66e9040eab2346c2196cc8f013f3
Daniel Pereira dos Santos <danielps99@gmail.com>
2017-03-07 18:02:55 -0300

    Diretiva showErrors cria tag html, mas não consegui sincronizar os erros do ngModel

--- src/app/shared/show-error.directive.ts
+++ src/app/shared/show-error.directive.ts
@@ -1,46 +1,42 @@
-import { Directive, HostListener, HostBinding,
-  ElementRef, Renderer, Input } from '@angular/core';
+import {
+  Directive, HostListener, HostBinding,
+  ElementRef, Renderer, Input
+} from '@angular/core';
 
-  import { NgModel } from '@angular/forms'; 
+import { NgModel } from '@angular/forms';
 
 @Directive({
   selector: 'input [showError]'
 })
 export class ShowErrorDirective {
 
-  @Input('showError') highlightColor: NgModel;
-
-  @HostListener('mouseenter') onMouseOver(){
-    /*this._renderer.setElementStyle(
-      this._elementRef.nativeElement,
-      'background-color', 'yellow'
-      );*/
-      this.backgroundColor = 'yellow';
+  // @Input('showError') highlightColor: NgModel;
+  @Input('showError') set highlightColora(ngModel: any){
+      console.log("NgModel ===>", ngModel);
   }
 
-  @HostListener('mouseleave') onMouseLeave(){
-    /*this._renderer.setElementStyle(
-      this._elementRef.nativeElement,
-      'background-color', 'white'
-      );*/
-      this.backgroundColor = 'white';
+  private nativeElement: Node;
+  constructor(private renderer: Renderer, private element: ElementRef) {
+    this.nativeElement = element.nativeElement;
   }
 
-  //@HostBinding('style.backgroundColor') backgroundColor: string;
-  @HostBinding('style.backgroundColor') get setColor(){
-    //codigo extra;
-    return this.backgroundColor;  
+  @HostListener('mouseenter') onMouseOver() {
+    let span = this.renderer.createElement(this.nativeElement, "span");
+    this.renderer.createText(span, "Click meddd!");
+    this.renderer.attachViewAfter(this.nativeElement, [span]);
+
+    //console.log("highlightColor", this.highlightColor.model);
+
+    this.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = 'white';
+  }
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    // console.log("style.backgroundColor') get setColor()");
+    return this.backgroundColor;
   }
   private backgroundColor: string;
-
-  constructor(
-    //private _elementRef: ElementRef,
-    //private _renderer: Renderer
-    ) { }
-
-    ngOnInit(){
-    console.log("this.highlightColor", this.highlightColor);
-  }
-
-
 }
